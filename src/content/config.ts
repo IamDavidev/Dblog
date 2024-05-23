1e884ff1d209694965fe5df07c57807fcb07a019
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-05-23 18:17:51 +0000

    feat: Update blog index page to display all blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,21 @@
 import { defineCollection, z } from 'astro:content'
 
+const schemaBlog = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+  heroImage: z.string().optional(),
+  tags: z.array(z.string()),
+  lang: z.string().optional().default('en'),
+  author: z.string().optional().default('David Lezam')
+})
+
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    tags: z.array(z.string()),
-    lang: z.string().optional().default('en')
-  })
+  schema: schemaBlog
 })
 
 export const collections = { blog }
+
+export type BlogPost = z.infer<typeof schemaBlog>

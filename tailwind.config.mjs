6603ffed0c18ccf6248b70985973b1268214db31
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-05-24 04:53:11 +0000

    feat: Update global.css and Tailwind configuration with new color variables and font faces

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,18 @@ export default {
     extend: {
       fontSize: {
         title: ['21rem']
+      },
+      colors: {
+        'primary-ligth': '#f3f4f6',
+        'primary-dark': '#1a1a1a',
+        'content-ligth': '#667085',
+        'content-dark': '#C0C5D0',
+        'accent-pink': '#FF0080',
+        'accent-dark-blue': '#363F72',
+        'acent-orange': 'C4320A',
+        'accent-yellow': '#FFD700',
+        'accent-green': '#027A48',
+        'accent-blue': '#3538C'
       }
     }
   },

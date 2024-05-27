1b1299f02b250572789364329f3fe29758f979d9
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-05-27 21:04:14 +0000

    feat: Update header and post card styles and classes

---
+++ src/lib/utils/colors.util.ts
@@ -0,0 +1,18 @@
+/*
+  'accent-pink': '#FF0080',
+        'accent-dark-blue': '#363F72',
+        'acent-orange': 'C4320A',
+        'accent-yellow': '#FFD700',
+        'accent-green': '#027A48',
+        'accent-blue': '#3538C'
+
+*/
+
+export const colorsWithOpacity = {
+  pink: {
+    bg: 'FDF2FA',
+    text: '#FF0080'
+  }
+}
+
+export type PickColor = keyof typeof colorsWithOpacity

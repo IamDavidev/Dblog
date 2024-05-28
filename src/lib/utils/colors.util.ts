227a9f7891d6f245e6087b0af529316b80c800c6
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-05-28 14:47:32 +0000

    feat: Update Tag component to use random color for each tag

--- src/lib/utils/colors.util.ts
+++ src/lib/utils/colors.util.ts
@@ -10,8 +10,24 @@
 
 export const colorsWithOpacity = {
   pink: {
-    bg: 'FDF2FA',
+    bg: '#FDF2FA',
     text: '#FF0080'
+  },
+  orange: {
+    bg: '#FFF6ED',
+    text: '#C4320A'
+  },
+  green: {
+    bg: '#ECFDF3',
+    text: '#027A48'
+  },
+  blue: {
+    bg: '#EEF4FF',
+    text: '#3538CD'
+  },
+  cyan: {
+    bg: '#F0F9FF',
+    text: '#026AA2'
   }
 }
 

21890d97962d77947b414cc3b9c96cfc8e978483
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-10-18 19:19:12 +0000

    refactor: Remove unused color definitions in colors.util.ts

--- src/lib/utils/colors.util.ts
+++ src/lib/utils/colors.util.ts
@@ -1,13 +1,3 @@
-/*
-  'accent-pink': '#FF0080',
-        'accent-dark-blue': '#363F72',
-        'acent-orange': 'C4320A',
-        'accent-yellow': '#FFD700',
-        'accent-green': '#027A48',
-        'accent-blue': '#3538C'
-
-*/
-
 export const colorsWithOpacity = {
   pink: {
     bg: '#FDF2FA',
